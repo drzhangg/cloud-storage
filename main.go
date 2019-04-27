f9b29c6ac00a432e0bdd48a7b4c43105eaff7d54
drzhang <654014730@qq.com>
2019-04-27 19:12:37 +0800

    处理文件上传页面

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"cloud-storage/handler"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/file/upload",handler.UploadHandler)
+	err:=http.ListenAndServe(":8080",nil)
+	if err != nil {
+		fmt.Printf("failed to start server,err:%s",err.Error())
+	}
+
+}
