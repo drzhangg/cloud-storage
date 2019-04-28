d19400cbaa873fb21c065e8544da403f21750b8a
drzhang <654014730@qq.com>
2019-04-28 10:56:14 +0800

    实现多个文件查询信息接口

--- handler/handler.go
+++ handler/handler.go
@@ -9,6 +9,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -33,37 +34,37 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 		defer file.Close()
 
 		fileMeta := meta.FileMeta{
-			FileName:head.Filename,
-			Location:"/tmp/" +head.Filename,
-			UploadAt:time.Now().Format("2006-01-02 15:04:05"),
+			FileName: head.Filename,
+			Location: "/tmp/" + head.Filename,
+			UploadAt: time.Now().Format("2006-01-02 15:04:05"),
 		}
 
 		//云端接收文件流，写入本地存储
 		newFile, err := os.Create(fileMeta.Location)
 		if err != nil {
-			fmt.Printf("Failed to create file,err:%s\n",err.Error())
+			fmt.Printf("Failed to create file,err:%s\n", err.Error())
 			return
 		}
 		defer newFile.Close()
 
 		//云端更新文件元信息集合
-		fileMeta.FileSize,err = io.Copy(newFile,file)
+		fileMeta.FileSize, err = io.Copy(newFile, file)
 		if err != nil {
-			fmt.Printf("Failed to save data into faile,err:%s\n",err.Error())
+			fmt.Printf("Failed to save data into faile,err:%s\n", err.Error())
 			return
 		}
-		newFile.Seek(0,0)
+		newFile.Seek(0, 0)
 		fileMeta.FileSha1 = util.FileSha1(newFile)
 		meta.UpdateFileMeta(fileMeta)
 
 		//保存返回正确信息
-		http.Redirect(w,r,"/file/upload/suc",http.StatusFound)
+		http.Redirect(w, r, "/file/upload/suc", http.StatusFound)
 	}
 }
 
 //UploadSucHandler:上传已完成
 func UploadSucHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w,"Upload finished!")
+	io.WriteString(w, "Upload finished!")
 }
 
 //GetFileMetaHandler:获取文件元信息
@@ -72,7 +73,20 @@ func GetFileMetaHandler(w http.ResponseWriter, r *http.Request) {
 
 	filehash := r.Form["filehash"][0]
 	fMeta := meta.GetFileMeta(filehash)
-	data,err := json.Marshal(fMeta)
+	data, err := json.Marshal(fMeta)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(data)
+}
+
+//FileQueryHandler:查询批量的文件元信息
+func FileQueryHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	limitCnt, _ := strconv.Atoi(r.Form.Get("limit"))
+	fileMetas := meta.GetLastFileMeta(limitCnt)
+	data,err := json.Marshal(fileMetas)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	http.HandleFunc("/file/upload",handler.UploadHandler)	//处理文件上传接口
 	http.HandleFunc("/file/upload/suc",handler.UploadSucHandler)		//文件上传成功接口
 	http.HandleFunc("/file/meta",handler.GetFileMetaHandler)
+	http.HandleFunc("/file/query",handler.FileQueryHandler)	//文件查询信息接口
 	err:=http.ListenAndServe(":8080",nil)
 	if err != nil {
 		fmt.Printf("failed to start server,err:%s",err.Error())
